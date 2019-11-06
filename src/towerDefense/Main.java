24f03cf03e252aa189de1828098cf925a4cb1796
thevsd <thevsd27@gmail.com>
2019-11-06 17:06:57 +0700

    PathTransition for moving enemies

--- src/towerDefense/Main.java
+++ src/towerDefense/Main.java
@@ -3,13 +3,18 @@ package towerDefense;
 import java.util.*;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import towerDefense.entity.enemies.NormalEnemy;
 
 public class Main extends Application {
     
@@ -22,38 +27,59 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
         Group root = new Group();
         layer = new Pane();
 
         root.getChildren().add(layer);
         scene = new Scene(root, (double)Config.SCREEN_WIDTH, (double)Config.SCREEN_HEIGHT);
-        
+
         primaryStage.setTitle(Config.GAME_NAME);
         primaryStage.setScene(scene);
         primaryStage.show();
 
         load();
-        gf.spawnEnemies(layer, n_Enemy);
+        //Get a single enemy for Testing
+        NormalEnemy e = gf.spawnEnemies(layer, n_Enemy);
+        ImageView t = e.getImageView();
 
-        AnimationTimer loop = new AnimationTimer(){
-        
-            @Override
-            public void handle(long now) {
-                gf.update();
-                // gf.removeSprites();
-            }
-        };
-        loop.start();
+
+        //Path Test
+        Path path = new Path();
+        path.getElements().add(new MoveTo(46 - 23, 0));
+        path.getElements().add(new LineTo(46 - 23, 4*46));
+        path.getElements().add(new LineTo(12*46 - 23, 4*46));
+        path.getElements().add(new LineTo(12*46 - 23, 10*46));
+        path.getElements().add(new LineTo(-46*2 - 23, 10*46));
+        path.getElements().add(new LineTo(-46*2 - 23, 14*46));
+        path.getElements().add(new LineTo(14*46 - 23, 14*46));
+        path.getElements().add(new LineTo(14*46 - 23, 18*46));
+
+        PathTransition pt = new PathTransition();
+        pt.setDuration(Duration.millis(20000));
+        pt.setPath(path);
+        pt.setNode(t);
+        pt.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pt.play();
+        //End Test
+
+//        AnimationTimer loop = new AnimationTimer(){
+//
+//            @Override
+//            public void handle(long now) {
+//                gf.update();
+//                // gf.removeSprites();
+//            }
+//        };
+//        loop.start();
     }
 
     public void load()
     {
-        map = new Image("/res/images/TowerDefense.png");
+        map = new Image("file:src/res/images/TowerDefense.png");
         ImageView mapBG = new ImageView(map);
         layer.getChildren().addAll(mapBG);
 
-        n_Enemy = new Image("/res/images/enemies/Normal.png");
+        n_Enemy = new Image("file:src/res/images/enemies/Normal.png");
     }
 
     public static void main(String[] args) {
